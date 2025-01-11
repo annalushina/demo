8d81604df4098373263de277aa7a05ef12ac0733
Anna Veretencheva <averetencheva@inno.tech>
2025-01-11 11:49:35 +0300

    very simple fix

--- src/main/java/ru/stepup/payments/mobile/MainApplication.java
+++ src/main/java/ru/stepup/payments/mobile/MainApplication.java
@@ -3,7 +3,8 @@ package ru.stepup.payments.mobile;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Третий коммит");
+        System.out.println("Третий коммит изменен");
         int b=10;
+        System.out.println("b= "+b);
     }
 }
