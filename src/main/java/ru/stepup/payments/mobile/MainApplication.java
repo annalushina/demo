deb208f26fe45b85a5625fc2780589dfd55866da
Anna Veretencheva <averetencheva@inno.tech>
2025-01-11 11:36:19 +0300

    удалена и добавлена новая строка

--- src/main/java/ru/stepup/payments/mobile/MainApplication.java
+++ src/main/java/ru/stepup/payments/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package ru.stepup.payments.mobile;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("11111");
+        System.out.println("Третий коммит");
+        int a=10;
     }
 }
